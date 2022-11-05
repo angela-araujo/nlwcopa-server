9ff26b4e982aa425ea9692bf73dd54ad5df0eaf1
Angela de Araujo <angela.araujo.rj@gmail.com>
2022-11-04 22:45:01 -0300

    criacao do schema do banco e novas funcionalidades

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors';
+import { z } from 'zod';
+import ShortUniqueId from 'short-unique-id'
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
@@ -20,13 +22,38 @@ async function bootstrap() {
         return { count }
     })
 
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        try {
+            const { title } = createPoolBody.parse(request.body);
+            const generate = new ShortUniqueId({ length: 6 });
+            const code = String(generate()).toUpperCase();
+
+            await prisma.pool.create({
+                data: {
+                    title,
+                    code
+                }
+            })
+
+            return reply.status(201).send({ code });
+
+        } catch (error) {
+            return reply.status(500).send({ error });
+        }
+
+    })
+
     fastify.get('/pools', async () => {
         const pools = await prisma.pool.findMany({
-            where: {
-                code: {
-                    startsWith: 'D'
-                }
-            }
+            // where: {
+            //     code: {
+            //         startsWith: 'D'
+            //     }
+            // }
         })
 
         return { pools }
